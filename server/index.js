ad7ca2828285f6e03119e063af530501cf0e2947
Karolis Ramanauskas <hello@karolisram.com>
2019-04-11 22:40:41 +0300

    Adjust prod server serving frontend

--- server/index.js
+++ server/index.js
@@ -9,9 +9,14 @@ app.get('/api/people', (req, res) => res.send([
 	'Maggie'
 ]))
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build/index.html'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+// }
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
